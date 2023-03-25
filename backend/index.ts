d5dcb14eaed91fba2db878b597c5b51226e8a5a3
Jacob Schuster <jschuster8765@gmail.com>
2023-03-25 17:54:55 -0400

    add environment varialbes

--- backend/index.ts
+++ backend/index.ts
@@ -1,17 +1,19 @@
 import express, { Application, Request, Response } from "express";
+import dotenv from "dotenv"
+
+dotenv.config({ path: "../.env" });
 
 const app: Application = express();
 
 app.get("/", async (req: Request, res: Response): Promise<Response> => {
   return res.status(200).send({
-    message: "Hello World!",
+    version: 0.1,
   });
 });
 
-const PORT = 3000;
 try {
-  app.listen(PORT, (): void => {
-    console.log(`Connected successfully on port ${PORT}`);
+  app.listen(process.env.PORT, (): void => {
+    console.log(`Connected successfully on port ${process.env.PORT}`);
   });
 } catch (error: any) {
   console.error(`Error occured: ${error.message}`);
