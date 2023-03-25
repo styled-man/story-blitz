3c8d94ac565770780326e513d4e6962cbe9994f6
Jacob Schuster <jschuster8765@gmail.com>
2023-03-25 18:07:17 -0400

    add router

--- backend/index.ts
+++ backend/index.ts
@@ -1,20 +1,13 @@
 import express, { Application, Request, Response } from "express";
 import dotenv from "dotenv"
+import { router } from "./routes/routes";
 
 dotenv.config({ path: "../.env" });
 
 const app: Application = express();
 
-app.get("/", async (req: Request, res: Response): Promise<Response> => {
-  return res.status(200).send({
-    version: 0.1,
-  });
-});
+app.use("/", router)
 
-try {
-  app.listen(process.env.PORT, (): void => {
-    console.log(`Connected successfully on port ${process.env.PORT}`);
-  });
-} catch (error: any) {
-  console.error(`Error occured: ${error.message}`);
-}
+app.listen(process.env.PORT, (): void => {
+  console.log(`Connected successfully on port ${process.env.PORT}`);
+});
