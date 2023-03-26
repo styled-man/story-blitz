519960dec41aca13931a6f743245af6f88652ad8
Jacob Schuster <jschuster8765@gmail.com>
2023-03-26 00:47:13 -0400

    formatting

--- backend/controllers/openai.ts
+++ backend/controllers/openai.ts
@@ -1,12 +1,12 @@
-import { Configuration, OpenAIApi } from "openai";
+import { Configuration, OpenAIApi } from "openai"
 import { WikipediaData } from "../interfaces/WikipediaData"
-import { PRE_PROMPT, POST_PROMPT } from "../config";
+import { PRE_PROMPT, POST_PROMPT } from "../config"
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
-});
+})
 
-const openai = new OpenAIApi(configuration);
+const openai = new OpenAIApi(configuration)
 
 export const generateData = async (data: WikipediaData[]) => {
     const firstPrompt: any = [{role: "user", content: `${PRE_PROMPT}\n${data.map((category: WikipediaData) => "\n" + category.section + "\n" + category.data)}`}]
@@ -16,11 +16,11 @@ export const generateData = async (data: WikipediaData[]) => {
         max_tokens: 3000,
         temperature: 0.5,
         stream: false
-    });
+    })
     const story = storyRaw.data.choices[0].message?.content
 
     const secondPrompt: any = [{role: "user", content: `${story}\n${POST_PROMPT}`}]
-    console.log("SECOND PROMPT\n\n\n", secondPrompt)
+    //console.log("SECOND PROMPT\n\n\n", secondPrompt)
 
     const questions_raw = await openai.createChatCompletion({
         model: "gpt-3.5-turbo",
@@ -28,9 +28,8 @@ export const generateData = async (data: WikipediaData[]) => {
         max_tokens: 3000,
         temperature: 0,
         stream: false
-    });
+    })
     let questions = questions_raw.data.choices[0].message!.content
-    console.log("\n\nQUESTIONS", questions)
     questions!.replace(/\n/g, "").replace(/\\/g, "")
     questions = JSON.parse(questions!)
 
