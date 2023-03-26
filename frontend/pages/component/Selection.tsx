e505359ea3b07e2e42def1d4b078cdc8aeff2ba7
David Ulloa <dulloa6310@gmail.com>
2023-03-26 00:53:59 -0400

    Selection page displays content of selected sections

--- frontend/pages/component/Selection.tsx
+++ frontend/pages/component/Selection.tsx
@@ -1,6 +1,9 @@
 import type { NextPage } from "next"
 import { useState } from "react"
 
+import PreviewWindow from "../../components/PreviewWindow"
+import Button from "../../components/Button"
+
 const Option = ({
     name,
     onChange,
@@ -13,7 +16,7 @@ const Option = ({
     isDisabled: boolean
 }) => {
     return (
-        <div className="text-2xl flex items-center gap-5">
+        <div className="text-lg flex items-center gap-5">
             <input
                 className="w-[20px] h-[20px] aspect-square"
                 type="checkbox"
@@ -50,24 +53,35 @@ const Selection: NextPage = () => {
     }
 
     return (
-        <main className="flex flex-col justify-center m-10 pt-10">
-            <h1 className="text-6xl py-10 text-center">{title}</h1>
-            <div className="flex flex-col gap-10 mx-20 pb-10">
-                {options.map((element, i) => {
-                    return (
-                        <Option
-                            name={element[0]}
-                            key={element[0]}
-                            checked={element[1]}
-                            isDisabled={isDisabled}
-                            onChange={(e: { currentTarget: { checked: boolean } }) => {
-                                options[i][1] = e.currentTarget.checked
-                                setOptions([...options])
-                                updateCount()
-                            }}
-                        />
-                    )
-                })}
+        <main className="flex flex-row justify-around m-4">
+            <div>
+                <h1 className="font-sans text-4xl font-bold">Found Article: {title}</h1>
+                <h2 className="font-sans text-xl">
+                    Please select the subheadings that will be included in your study.
+                </h2>
+                <div className="flex flex-col mt-4">
+                    {options.map((element, i) => {
+                        return (
+                            <Option
+                                name={element[0]}
+                                key={element[0]}
+                                checked={element[1]}
+                                isDisabled={isDisabled}
+                                onChange={(e: { currentTarget: { checked: boolean } }) => {
+                                    options[i][1] = e.currentTarget.checked
+                                    setOptions([...options])
+                                    updateCount()
+                                }}
+                            />
+                        )
+                    })}
+                    <Button text="Next" className=" border-black border-2 w-36 mt-4" />
+                </div>
+            </div>
+            <div>
+                <PreviewWindow
+                    sections={[{ title: "Woah!", content: "This is the content of my section" }]}
+                />
             </div>
         </main>
     )
