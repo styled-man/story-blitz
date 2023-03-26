60ffafeaa1b3dd649a5438b8380765a9e5035e2a
David Ulloa <dulloa6310@gmail.com>
2023-03-26 08:28:24 -0400

    Added selectedSections into context provider

--- frontend/pages/selection.tsx
+++ frontend/pages/selection.tsx
@@ -6,43 +6,48 @@ import PreviewWindow from "../components/PreviewWindow"
 import Button from "../components/Button"
 import SelectionOption from "../components/SectionOption"
 import Checkbox from "../components/Checkbox"
+import { useQuizContext } from "../hooks/QuizContext"
 
 const Selection: NextPage = props => {
+    const { sections, articleName, selectedSections, setSelectedSections } = useQuizContext()
+
     const router = useRouter()
-    const [title, setTitle] = useState("")
-    const [sections, setSections] = useState([])
 
     const maxSelection = 2
 
-    const [selectedCount, setSelectedCount] = useState<number>(0)
-    const [isDisabled, setIsDisabled] = useState<boolean>(false)
-
-    let selected_options = new Set()
-
     function onCheckChange(checked: boolean, label: string) {
-        if (checked) {
-            selected_options.add(label)
-        } else {
-            selected_options.delete(label)
+        if (checked && selectedSections.length < 2) {
+            setSelectedSections(prev => [...prev, label])
+        } else if (!checked) {
+            let array = selectedSections
+            let index = selectedSections.indexOf(label)
+            array.splice(index, 1)
+            setSelectedSections([...array])
         }
     }
 
     return (
         <main className="flex flex-row justify-around m-4">
             <div>
-                <h1 className="font-sans text-4xl font-bold">Found Article: {title}</h1>
+                <h1 className="font-sans text-4xl font-bold">Found Article: {articleName}</h1>
                 <h2 className="font-sans text-xl">
                     Please select the subheadings that will be included in your study.
                 </h2>
                 <div className="flex flex-col mt-4">
                     {sections.map(element => {
-                        return <Checkbox onCheckedChange={onCheckChange} label={element?.title} />
+                        return (
+                            <Checkbox
+                                onCheckedChange={onCheckChange}
+                                label={element?.title}
+                                selectedSections={selectedSections}
+                            />
+                        )
                     })}
                     <Button text="Next" className=" border-black border-2 w-36 mt-4" />
                 </div>
             </div>
             <div>
-                <PreviewWindow sections={sections} />
+                <PreviewWindow sections={sections} selectedSections={selectedSections} />
             </div>
         </main>
     )
