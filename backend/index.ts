dda1ce9d40e61158939e73ddea260e2ad3c475d9
Jacob Schuster <jschuster8765@gmail.com>
2023-03-26 01:41:08 -0400

    cors

--- backend/index.ts
+++ backend/index.ts
@@ -2,11 +2,13 @@ import express, { Application, Request, Response } from "express"
 import dotenv from "dotenv"
 import bodyParser from "body-parser"
 import { router } from "./routes/routes"
+import cors from "cors"
 
 dotenv.config({ path: "../.env" })
 
 const app: Application = express()
 app.use(bodyParser.json())
+app.use(cors())
 
 app.use("/", router)
 
