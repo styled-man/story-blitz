3dd4fa08a411f79efb3ad2d9d5775c4251d011a5
02danieljonas <02danieljonas@gmail.com>
2023-03-25 23:34:09 -0400

    selection page

---
+++ frontend/pages/selection.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from "next"
+import Head from "next/head"
+import Image from "next/image"
+import styles from "../styles/Home.module.css"
+import Selection from "./component/Selection"
+
+const selection: NextPage = () => {
+    return (
+        <>
+            <Selection />
+        </>
+    )
+}
+
+export default selection
