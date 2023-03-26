cdf76e7d9ed13407d611b820681311974d4bb3a5
David Ulloa <dulloa6310@gmail.com>
2023-03-26 06:58:08 -0400

    Removed fetch from section page

--- frontend/pages/selection.tsx
+++ frontend/pages/selection.tsx
@@ -5,47 +5,26 @@ import { useRouter } from "next/router"
 import PreviewWindow from "../components/PreviewWindow"
 import Button from "../components/Button"
 import SelectionOption from "../components/SectionOption"
+import Checkbox from "../components/Checkbox"
 
 const Selection: NextPage = props => {
     const router = useRouter()
-    const [title, setTitle] = useState<string>("")
-
-    useEffect(() => {
-        async function getData() {
-            try {
-                const { keywords } = router.query
-                if (keywords == undefined) {
-                    return
-                }
-                let data = await fetch(`http://localhost:6969/article?keywords=${keywords}`, {
-                    method: "GET",
-                })
-                let jsonData = await data.json()
-                console.log(jsonData)
-            } catch (error) {
-                console.log(error)
-            }
-        }
-
-        getData()
-    }, [router.query])
+    const [title, setTitle] = useState("")
+    const [sections, setSections] = useState([])
 
     const maxSelection = 2
 
     const [selectedCount, setSelectedCount] = useState<number>(0)
     const [isDisabled, setIsDisabled] = useState<boolean>(false)
 
-    const [options, setOptions] = useState<[string, boolean][]>([
-        ["Signs and symptoms", false],
-        ["Risk Factor", false],
-        ["Causes and mechanism", false],
-        ["Diagnosis", false],
-        ["Prevention", false],
-        ["Management", false],
-    ])
+    let selected_options = new Set()
 
-    const updateCount = () => {
-        setIsDisabled(options.filter(e => e[1]).length == maxSelection)
+    function onCheckChange(checked: boolean, label: string) {
+        if (checked) {
+            selected_options.add(label)
+        } else {
+            selected_options.delete(label)
+        }
     }
 
     return (
@@ -56,28 +35,14 @@ const Selection: NextPage = props => {
                     Please select the subheadings that will be included in your study.
                 </h2>
                 <div className="flex flex-col mt-4">
-                    {options.map((element, i) => {
-                        return (
-                            <SelectionOption
-                                name={element[0]}
-                                key={element[0]}
-                                checked={element[1]}
-                                isDisabled={isDisabled}
-                                onChange={(e: { currentTarget: { checked: boolean } }) => {
-                                    options[i][1] = e.currentTarget.checked
-                                    setOptions([...options])
-                                    updateCount()
-                                }}
-                            />
-                        )
+                    {sections.map(element => {
+                        return <Checkbox onCheckedChange={onCheckChange} label={element?.title} />
                     })}
                     <Button text="Next" className=" border-black border-2 w-36 mt-4" />
                 </div>
             </div>
             <div>
-                <PreviewWindow
-                    sections={[{ title: "Woah!", content: "This is the content of my section" }]}
-                />
+                <PreviewWindow sections={sections} />
             </div>
         </main>
     )
